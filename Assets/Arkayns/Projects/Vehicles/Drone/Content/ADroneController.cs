e2bdd8e82f41ee003a37ff08f80b486551479f05
Serge Maia <sergemaia9@gmail.com>
2022-09-28 21:11:09 +0100

    REF: Adding smooth rotation

--- Assets/Arkayns/Projects/Vehicles/Drone/Content/ADroneController.cs
+++ Assets/Arkayns/Projects/Vehicles/Drone/Content/ADroneController.cs
@@ -8,13 +8,19 @@ namespace Arkayns.Projects.Vehicles.Drone {
     public class ADroneController : ABaseRigidbody {
 
         [Header("Control Properties")] 
-        [SerializeField] private float m_minMaxPitxh = 30f;
+        [SerializeField] private float m_minMaxPitch = 30f;
         [SerializeField] private float m_minMaxRoll = 30f;
         [SerializeField] private float m_yawPower = 4f;
+        [SerializeField] private float m_lerpSpeed = 2f;
 
         private ADroneInput m_input;
         private List<IEngine> m_engines = new List<IEngine>();
 
+        private float m_finalPitch;
+        private float m_finalRoll;
+        private float m_finalYaw;
+        private float m_yaw;
+        
         public void Start() {
             m_input = GetComponent<ADroneInput>();
             m_engines = GetComponentsInChildren<IEngine>().ToList();
@@ -26,13 +32,21 @@ namespace Arkayns.Projects.Vehicles.Drone {
         } // HandlePhysics
 
         protected virtual void HandleEngine() {
-            foreach (var engine in m_engines) 
-                engine.UpdateEngine(m_rb, m_input, (sbyte)m_engines.Count);
-            // m_rb.AddForce(Vector3.up * (m_rb.mass * Physics.gravity.magnitude));
+            foreach (var engine in m_engines) engine.UpdateEngine(m_rb, m_input, (sbyte)m_engines.Count);
         } // HandleEngine
         
         protected virtual void HandleControl() {
+            var time = Time.deltaTime * m_lerpSpeed;
+            var pitch = m_input.Cyclic.y * m_minMaxPitch;
+            var roll = m_input.Cyclic.x * m_minMaxRoll;
+            m_yaw += m_input.Pedal * m_yawPower;
             
+            m_finalPitch = Mathf.Lerp(m_finalPitch, pitch, time);
+            m_finalRoll = Mathf.Lerp(m_finalRoll, roll, time);
+            m_finalYaw = Mathf.Lerp(m_finalYaw, m_yaw, time);
+            
+            var rot = Quaternion.Euler(m_finalPitch, m_finalYaw, m_finalRoll);
+            m_rb.MoveRotation(rot);
         } // HandleControl
         
     } // Class ADroneController
