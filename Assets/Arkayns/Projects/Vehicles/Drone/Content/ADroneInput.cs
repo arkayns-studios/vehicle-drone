fe094e2f6c0bcc4f3f919433db8c3399809888b4
Serge Maia <sergemaia9@gmail.com>
2022-09-28 21:40:10 +0100

    ADD: Propeller Physics & Invert Control Input

--- Assets/Arkayns/Projects/Vehicles/Drone/Content/ADroneInput.cs
+++ Assets/Arkayns/Projects/Vehicles/Drone/Content/ADroneInput.cs
@@ -9,20 +9,25 @@ namespace Arkayns.Projects.Vehicles.Drone {
         private float m_pedal;
         private float m_throttle;
 
+        public bool invertControl;
+        
         public Vector2 Cyclic => m_cyclic;
         public float Pedal => m_pedal;
         public float Throttle => m_throttle;
 
         private void OnCyclic(InputValue value) {
-            m_cyclic = value.Get<Vector2>();
+            if (invertControl) m_cyclic = -value.Get<Vector2>();
+            else m_cyclic = value.Get<Vector2>();
         } // OnCyclic
         
         private void OnPedal(InputValue value) {
-            m_pedal = value.Get<float>();
+            if (invertControl) m_pedal = -value.Get<float>();
+            else m_pedal = value.Get<float>();
         } // OnPedal
         
         private void OnThrottle(InputValue value) {
-            m_throttle = value.Get<float>();
+            if (invertControl) m_throttle = -value.Get<float>();
+            else m_throttle = value.Get<float>();
         } // OnThrottle
         
     } // Class ADroneInput
--- Assets/Arkayns/Projects/Vehicles/Drone/Content/AEngine.cs
+++ Assets/Arkayns/Projects/Vehicles/Drone/Content/AEngine.cs
@@ -7,6 +7,10 @@ namespace Arkayns.Projects.Vehicles.Drone {
 
         [Header("Engine Properties")]
         [SerializeField] private float m_maxPower = 4f;
+
+        [Header("Propeller Properties")] 
+        [SerializeField] private Transform propeller;
+        [SerializeField] private float propRotationSpeed = 300f;
         
         public void InitEngine() {
             
@@ -23,7 +27,14 @@ namespace Arkayns.Projects.Vehicles.Drone {
             var engineForce = Vector3.zero;
             engineForce = transform.up * ((rb.mass * Physics.gravity.magnitude + finalDiff) + (input.Throttle * m_maxPower)) / engines;
             rb.AddForce(engineForce, ForceMode.Force);
+
+            HandlePropeller();
         } // UpdateEngine
+
+        private void HandlePropeller() {
+            if (!propeller) return;
+            propeller.Rotate(Vector3.up, propRotationSpeed);
+        } // HandlePropeller
         
     } // Class AEngine
     
